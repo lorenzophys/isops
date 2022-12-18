9cfa54ab9871e7d602b71b05aa7f59a77f7984b4
Lorenzo Maffioli <lorenzo.maffioli@gmail.com>
2022-12-18 16:35:31 +0100

    moved fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,24 +10,6 @@ TESTS_PATH = os.path.dirname(os.path.realpath(__file__))
 SAMPLES_PATH = os.path.join(TESTS_PATH, "samples")
 
 
-@pytest.fixture(scope="function")
-def simple_dir_struct(tmp_path, example_dotspos_yaml):
-    """Nobody forbids me to make a fixture that returns a function"""
-
-    def _internal(yaml_to_test, config=example_dotspos_yaml):
-        yaml = YAML(typ="safe")
-
-        dotsops = tmp_path / "root/.sops.yaml"
-        dotsops.parent.mkdir()
-        yaml.dump(config, dotsops)
-        secret = tmp_path / "root/secret.yaml"
-        yaml.dump(yaml_to_test, secret)
-        root = tmp_path / "root"
-        return str(dotsops), str(secret), str(root), yaml_to_test
-
-    return _internal
-
-
 @pytest.fixture(scope="module")
 def example_good_deploy_yaml():
     path = Path(os.path.join(SAMPLES_PATH, "good_deploy_yaml.yaml"))
@@ -92,3 +74,21 @@ def dot_sops_bad_encrypted_regex():
 def dot_sops_one_rule():
     path = Path(os.path.join(SAMPLES_PATH, ".sops_one_rule.yaml"))
     return load_yaml(path)
+
+
+@pytest.fixture(scope="function")
+def simple_dir_struct(tmp_path, example_dotspos_yaml):
+    """Nobody forbids me to make a fixture that returns a function"""
+
+    def _internal(yaml_to_test, config=example_dotspos_yaml):
+        yaml = YAML(typ="safe")
+
+        dotsops = tmp_path / "root/.sops.yaml"
+        dotsops.parent.mkdir()
+        yaml.dump(config, dotsops)
+        secret = tmp_path / "root/secret.yaml"
+        yaml.dump(yaml_to_test, secret)
+        root = tmp_path / "root"
+        return str(dotsops), str(secret), str(root), yaml_to_test
+
+    return _internal
